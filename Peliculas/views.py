1565cbfedc8abfd3f6e108fe8431a614b1ba53c2
eldux123 <eeguerrerr@eafit.edu.co>
2025-08-01 11:12:02 -0500

    cambio en views.py

--- Peliculas/views.py
+++ Peliculas/views.py
@@ -7,4 +7,5 @@ def home(request):
  #return render (request, 'home.html')#
  return render (request, 'home.html',{'name': 'Erick Guerrero'})
 def about(request):
-    return HttpResponse("<h1>Acerca de esta aplicación</h1>")
+    #return HttpResponse("<h1>Acerca de esta aplicación</h1>")#
+ return render (request, 'about.html',{'name': 'Erick Guerrero'})
